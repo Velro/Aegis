2d6b081c65fd54a57ff39dbbe051701e5f8f624d
James Fulop <jameslynnfulop@gmail.com>
2014-01-05 19:35:27 -0500

    menus

--- Aegis/Assets/Scripts/GUI/UpgroidsGUI.js
+++ Aegis/Assets/Scripts/GUI/UpgroidsGUI.js
@@ -5,61 +5,81 @@ public static var selections = new Array();
 var allUpgrades : Upgrades[];
 var unlockedUpgrades = new Array();
 var convUnlockedUpgrades : Upgrades[];
-
+var toBeBought : int;
+var confirmEquipPurchaseBool : boolean = false;
+var toBeEquipped : int;
+var equipSelectionBool : boolean = false;
 var supportBayRect : Rect = Rect(20, 20, 400, 400);
 var confirmStarMap : boolean = false;
 
+var physicalWeaponsRect : Rect = new Rect(0,0,Screen.width/8,Screen.height-(Screen.height/4));
+var energyWeaponsRect : Rect = new Rect(0,0,Screen.width/8,Screen.height - Screen.height/4);
+var explosiveWeaponsRect : Rect = new Rect(0,0,Screen.width/8,Screen.height - Screen.height/4);
+var devicesRect : Rect = new Rect(0,0,Screen.width/4,Screen.height - Screen.height/4);
+var buttonRect : Rect = new Rect(0,0,Screen.width/8-10,60); 
+physicalWeaponsRect.center = new Vector2(Screen.width/8, Screen.height/2);
+energyWeaponsRect.center = new Vector2((Screen.width/8)*2+15, Screen.height/2);
+explosiveWeaponsRect.center = new Vector2((Screen.width/8)*3 + 30, Screen.height/2);
+devicesRect.center = new Vector2((Screen.width/8)*5, Screen.height/2);
+supportBayRect.center = new Vector2(Screen.width/2, Screen.height/2);
 enum Menu {
 	SupportBay,
 	EquipEquipables,
 	BuyEquipables,
 	BuyUpgrades
 }
+
 var currentState : Menu = Menu.SupportBay;
 
 class Upgrades extends System.Object{
 	var name : String;
 	var cost : int;
+	var description : String;
 	var bool : boolean = false;
 	var bought : boolean = false;
 	var selected : boolean = false;
-}
-
-function Awake (){
-	//selections.Push("Vulcan Cannon");
-	//selections.Push("Fusion Beam");
 }
 
 function Start (){
 	width = Screen.width;
 	height = Screen.height;
 }
-
+var popupRect = Rect(400,300,250,150);
 function OnGUI (){
 	Title();
-	//Shop();
-	//Unlocked();
 	if (confirmStarMap == true){
-		var rect = Rect(400,300,250,150);
-		GUI.Window(1, rect, ConfirmStarMap, "Confirm");
+		GUI.Window(1, popupRect, ConfirmStarMap, "Confirm");
 	}
-		
+	if (confirmEquipPurchaseBool == true){
+		GUI.Window(2, popupRect, ConfirmEquipPurchase, "Confirm");
+	}
+	if (equipSelectionBool == true){
+		GUI.Window(3, popupRect, EquipSelection, "Bind Equipment");
+	}
 	switch (currentState){
 		case Menu.SupportBay:
-		GUI.Window(0,supportBayRect, SupportBay, "Support Bay");
+			GUI.Window(0,supportBayRect, SupportBay, "Support Bay");
+			break;
 		case Menu.EquipEquipables:
-		Equips();
+			Equips();
+			if (Input.GetButtonDown("Pause"))currentState = Menu.SupportBay;
+			break;
 		case Menu.BuyEquipables:
-		BuyEquips();
+			BuyEquips();
+			if (Input.GetButtonDown("Pause"))currentState = Menu.SupportBay;
+			break;
 		case Menu.BuyUpgrades:
-		BuyUpgrades();
+			BuyUpgrades();
+			if (Input.GetButtonDown("Pause"))currentState = Menu.SupportBay;
+			break;
 	}
-	
+	ButtonLabels();
 }
 
 function Title (){
-	GUI.Label(Rect(width/2 - 100,10,200,20),"Choose your loadout");
-	GUI.Label(Rect(width/2+100, 10,200, 20), "Total Credits: "+PlayerStats.totalCredits);
+	var creditsRect : Rect = new Rect(0,0,140,20);
+	creditsRect.center = new Vector2(Screen.width/2, 20);
+	GUI.Label(creditsRect, "Total Credits: "+PlayerStats.totalCredits);
 }
 
 function SupportBay(){
@@ -86,70 +106,178 @@ function SupportBay(){
 }
 
 function Equips (){
-	if (unlockedUpgrades.length > 0){
-		convUnlockedUpgrades = unlockedUpgrades.ToBuiltin(Upgrades) as Upgrades[];
-		//Debug.Log(convUnlockedUpgrades[0].name);
-		GUILayout.BeginArea(Rect (10, Screen.height/2, 400,400));
-			GUILayout.BeginHorizontal();
-			//Debug.Log(convUnlockedUpgrades.length);
-			if (convUnlockedUpgrades.length > 0){
-				for (var i : int; i < convUnlockedUpgrades.length; i++){
-					convUnlockedUpgrades[i].selected = GUILayout.Toggle(convUnlockedUpgrades[i].selected, convUnlockedUpgrades[i].name, "Button");
-					if (convUnlockedUpgrades[i].selected == true){
-						var str : String = convUnlockedUpgrades[i].name;
-						var bool = false;
-						for (var select in selections){
-							if (str == select)bool = true;
-						}
-						if (bool == false)selections.Push(str);
-					}
-					if (selections.length == 2)Debug.Log(selections[0]+"   "+selections[1]);
-				}
+	GUILayout.Label("Unlocked Equipment");
+	GUI.Box(physicalWeaponsRect,"Physical Weapons");
+	GUI.Box(energyWeaponsRect, "Energy Weapons");
+	GUI.Box(explosiveWeaponsRect, "Explosive Weapons");
+	GUI.Box(devicesRect, "Devices");
+	/****Physical Weapons ****/
+	for (var i : int = 0; i < 4; i++){
+		if (allUpgrades[i].bought == true){
+			var strA : String = allUpgrades[i].name;
+			buttonRect.center = Vector2(physicalWeaponsRect.center.x, Screen.height/4 + (i * 80));
+			/**** To Be Equipped ****/
+			allUpgrades[i].bool = GUI.Toggle(buttonRect,allUpgrades[i].bool,strA,"Button");
+			/**** Equipping ****/
+			if (allUpgrades[i].bool == true && allUpgrades[i].selected == false){
+				toBeEquipped = i;
+				equipSelectionBool = true;
 			}
-			GUILayout.EndHorizontal();
-		GUILayout.EndArea();
+			/**** Equipped ****/
+			if (allUpgrades[i].bought == true){
+				allUpgrades[i].bool = true;
+			}
+		}
 	}
-}
-function BuyEquips (){
-	GUILayout.BeginArea(Rect(10,50,width-40,height - 40));
-		GUILayout.Label("Shop");
-		
-		GUILayout.BeginHorizontal();
-		GUILayout.Label("Weapons");
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-			for (var i : int = 0; i < 4; i++){
-				var str : String = allUpgrades[i].name+"  "+allUpgrades[i].cost;
-				if (allUpgrades[i].bool == false){
-					allUpgrades[i].bool = GUILayout.Toggle(allUpgrades[i].bool,str,"Button");
-				}  
-				if (allUpgrades[i].bool == true && allUpgrades[i].bought == false){
-					PlayerStats.totalCredits -= allUpgrades[i].cost;
-					unlockedUpgrades.Push(allUpgrades[i]);
-					//Debug.Log(unlockedUpgrades.length);
-					allUpgrades[i].bought = true;
-				}
-				if (allUpgrades[i].bought == true){
-					allUpgrades[i].bool = GUILayout.Toggle(allUpgrades[i].bool,str,"Button");
-					allUpgrades[i].bool = true;
-				}
+	/**** Energy Weapons ****/
+	for (var j : int = 4; j < 8; j++){
+		var str1 : String = allUpgrades[j].name;
+		buttonRect.center = Vector2(energyWeaponsRect.center.x, Screen.height/4 + ((j - 4) * 80));
+		/**** To Be Bought ****/
+		allUpgrades[j].bool = GUI.Toggle(buttonRect,allUpgrades[j].bool,str1,"Button");
+		/**** Buying ****/
+		if (allUpgrades[j].bool == true && allUpgrades[j].bought == false){
+			toBeBought = j;
+			confirmEquipPurchaseBool = true;
+		}
+		/**** Bought ****/
+		if (allUpgrades[j].bought == true){
+			allUpgrades[j].bool = true;
+		}
+	}
+	/**** Explosive Weapons ****/
+	for (var k : int = 8; k < 12; k++){
+		var str2 : String = allUpgrades[k].name;
+		buttonRect.center = Vector2(explosiveWeaponsRect.center.x, Screen.height/4 + ((k - 8) * 80));
+		/**** To Be Bought ****/
+		allUpgrades[k].bool = GUI.Toggle(buttonRect,allUpgrades[k].bool,str2,"Button"); 
+		/**** Buying ****/
+		if (allUpgrades[k].bool == true && allUpgrades[k].bought == false){
+			toBeBought = k;
+			confirmEquipPurchaseBool = true;
+		}
+		/**** Bought ****/
+		if (allUpgrades[k].bought == true){;
+			allUpgrades[k].bool = true;
+		}
+	}
+	/*
+	for (var i : int; i < convUnlockedUpgrades.length; i++){
+		convUnlockedUpgrades[i].selected = GUILayout.Toggle(convUnlockedUpgrades[i].selected, convUnlockedUpgrades[i].name, "Button");
+		if (convUnlockedUpgrades[i].selected == true){
+			var str : String = convUnlockedUpgrades[i].name;
+			var bool = false;
+			for (var select in selections){
+				if (str == select)bool = true;
 			}
-		GUILayout.EndHorizontal();
-		GUILayout.Label("Devices");
-		GUILayout.BeginHorizontal();
-		
-		GUILayout.EndHorizontal();
-	
-		GUILayout.Label("Stats");
-	GUILayout.EndArea();
+			if (bool == false)selections.Push(str);
+		}
+		//if (selections.length == 2)Debug.Log(selections[0]+"   "+selections[1]);
+	}
+	*/
+}
+
+function BuyEquips (){
+	GUILayout.Label("Equipment Shop");
+	GUI.Box(physicalWeaponsRect,"Physical Weapons");
+	GUI.Box(energyWeaponsRect, "Energy Weapons");
+	GUI.Box(explosiveWeaponsRect, "Explosive Weapons");
+	GUI.Box(devicesRect, "Devices");
+	/****Physical Weapons ****/
+	for (var i : int = 0; i < 4; i++){
+		var str : String = allUpgrades[i].name;
+		buttonRect.center = Vector2(physicalWeaponsRect.center.x, Screen.height/4 + (i * 80));
+		/**** To Be Bought ****/
+		allUpgrades[i].bool = GUI.Toggle(buttonRect,allUpgrades[i].bool,str,"Button");
+		/**** Buying ****/
+		if (allUpgrades[i].bool == true && allUpgrades[i].bought == false){
+			toBeBought = i;
+			confirmEquipPurchaseBool = true;
+		}
+		/**** Bought ****/
+		if (allUpgrades[i].bought == true){
+			allUpgrades[i].bool = true;
+		}
+	}
+	/**** Energy Weapons ****/
+	for (var j : int = 4; j < 8; j++){
+		var str1 : String = allUpgrades[j].name;
+		buttonRect.center = Vector2(energyWeaponsRect.center.x, Screen.height/4 + ((j - 4) * 80));
+		/**** To Be Bought ****/
+		allUpgrades[j].bool = GUI.Toggle(buttonRect,allUpgrades[j].bool,str1,"Button");
+		/**** Buying ****/
+		if (allUpgrades[j].bool == true && allUpgrades[j].bought == false){
+			toBeBought = j;
+			confirmEquipPurchaseBool = true;
+		}
+		/**** Bought ****/
+		if (allUpgrades[j].bought == true){;
+			allUpgrades[j].bool = true;
+		}
+	}
+	/**** Explosive Weapons ****/
+	for (var k : int = 8; k < 12; k++){
+		var str2 : String = allUpgrades[k].name;
+		buttonRect.center = Vector2(explosiveWeaponsRect.center.x, Screen.height/4 + ((k - 8) * 80));
+		/**** To Be Bought ****/
+		allUpgrades[k].bool = GUI.Toggle(buttonRect,allUpgrades[k].bool,str2,"Button");
+		/**** Buying ****/
+		if (allUpgrades[k].bool == true && allUpgrades[k].bought == false){
+			toBeBought = k;
+			confirmEquipPurchaseBool = true;
+		}
+		/**** Bought ****/
+		if (allUpgrades[k].bought == true){
+			allUpgrades[k].bool = true;
+		}
+	}
 }
 function BuyUpgrades(){
 
 }
 function ConfirmStarMap(windowID : int){
 	if (GUI.Button(Rect(10,25,110,70), "Go to star map?"))
-		Application.LoadLevel("Star Map");
+		Application.LoadLevel("Star Map");		
 	if (GUI.Button(Rect(125,25,110,70), "Cancel"))
 		confirmStarMap = false;
+}
+function ConfirmEquipPurchase(windowID : int){
+	var str : String = "Purchase "+allUpgrades[toBeBought].name+" for "+allUpgrades[toBeBought].cost+"?";
+	GUILayout.BeginVertical();
+	GUILayout.Label(str);
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Confirm")){
+			PlayerStats.totalCredits -= allUpgrades[toBeBought].cost;
+			unlockedUpgrades.Push(allUpgrades[toBeBought]);
+			allUpgrades[toBeBought].bought = true;
+			confirmEquipPurchaseBool = false;
+		}
+		if (GUILayout.Button("Cancel")){
+			allUpgrades[toBeBought].bool = false;
+			confirmEquipPurchaseBool = false;
+		}
+		GUILayout.EndHorizontal();
+	GUILayout.EndVertical();
+}
+function EquipSelection (windowID : int){
+	var str : String = "Bind "+allUpgrades[toBeBought].name+" to ";
+	GUILayout.BeginVertical();
+	GUILayout.Label(str);
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Confirm")){
+			PlayerStats.totalCredits -= allUpgrades[toBeBought].cost;
+			unlockedUpgrades.Push(allUpgrades[toBeBought]);
+			allUpgrades[toBeBought].bought = true;
+			equipSelectionBool = false;
+		}
+		if (GUILayout.Button("Cancel")){
+			allUpgrades[toBeBought].bool = false;
+			equipSelectionBool = false;
+		}
+		GUILayout.EndHorizontal();
+	GUILayout.EndVertical();
+}
+function ButtonLabels (){
+	GUI.Label(Rect(Screen.width - 200, Screen.height - 50, 80, 40), "A - Select", "Button"); 
+	GUI.Label(Rect(Screen.width - 100, Screen.height - 50, 80, 40), "B - Back", "Button");
 }

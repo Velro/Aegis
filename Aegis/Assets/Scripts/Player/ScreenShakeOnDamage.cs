f7e05b23d32435f6d7b0f1e044541385df906ef5
James Fulop <jameslynnfulop@gmail.com>
2014-04-08 04:26:47 -0700

    vertical shaking

--- Aegis/Assets/Scripts/Player/ScreenShakeOnDamage.cs
+++ Aegis/Assets/Scripts/Player/ScreenShakeOnDamage.cs
@@ -3,41 +3,70 @@ using System.Collections;
 
 public class ScreenShakeOnDamage : MonoBehaviour 
 {
+    Vector3 originalPosition;
     private float currentHealth;
-    public float xShakeFrequency  = 0.02f;
+    public float   xShakeFrequency  = 0.02f;
     public Vector2 xShakeMagnitude = new Vector2(-1,1);
-    public float shakeDuration = 0.2f;
-    private float shakeStartTime = 0;
-    bool shaking = false;
+    public float   xShakeDuration = 0.2f;
+    private float  xShakeStartTime = 0;
+    public float   yShakeFrequency = 0.02f;
+    public Vector2 yShakeMagnitude = new Vector2(-1, 1);
+    public float   yShakeDuration = 0.2f;
+    private float  yShakeStartTime = 0;
+    bool shakingHorizontal = false;
+    bool shakingVertical = false;
 
     void Start () 
     {
         currentHealth = GetComponent<PlayerStats>().Health;
+        originalPosition = Camera.main.transform.localPosition;
     }
 
     void Update () 
     {
 	    if (GetComponent<PlayerStats>().Health < currentHealth)
         {
-		    shaking = true;
-		    shakeStartTime = Time.time;
+		    shakingHorizontal = true;
+            shakingVertical = true;
+		    xShakeStartTime = Time.time;
+            yShakeStartTime = Time.time;
 		    currentHealth = GetComponent<PlayerStats>().Health;
 	    }
-	    if (shaking)
+	    if (shakingHorizontal)
         {
-		    StartCoroutine(ScreenShake());
+		    StartCoroutine(ScreenShakeX());
 	    }
-	    if (shaking && Time.time > shakeStartTime + shakeDuration)
+        if (shakingVertical)
         {
-		    shaking = false;
-            Camera.main.transform.localPosition = new Vector3(0, Camera.main.transform.localPosition.y, 0);
+            StartCoroutine(ScreenShakeY());
+        }
+	    if (shakingHorizontal && Time.time > xShakeStartTime + xShakeDuration)
+        {
+		    shakingHorizontal = false;
+            Camera.main.transform.localPosition = originalPosition;
 	    }
+        if (shakingVertical && Time.time > xShakeStartTime + xShakeDuration)
+        {
+            shakingVertical = false;
+            Camera.main.transform.localPosition = originalPosition;
+        }
     }
-    IEnumerator ScreenShake()
+
+    IEnumerator ScreenShakeX()
     {
         var shakePos = Random.Range(xShakeMagnitude.x, xShakeMagnitude.y);
-        Camera.main.transform.localPosition = new Vector3(Camera.main.transform.localPosition.x + shakePos, Camera.main.transform.localPosition.y, Camera.main.transform.localPosition.z);
+        Camera.main.transform.localPosition = new Vector3( Camera.main.transform.localPosition.x + shakePos, 
+                                                           Camera.main.transform.localPosition.y, 
+                                                           Camera.main.transform.localPosition.z );
         yield return new WaitForSeconds(xShakeFrequency);
     }
 
+    IEnumerator ScreenShakeY()
+    {
+        var shakePos = Random.Range(yShakeMagnitude.x, yShakeMagnitude.y);
+        Camera.main.transform.localPosition = new Vector3( Camera.main.transform.localPosition.x,
+                                                           Camera.main.transform.localPosition.y,
+                                                           Camera.main.transform.localPosition.z + shakePos);
+        yield return new WaitForSeconds(xShakeFrequency);
+    }
 }
